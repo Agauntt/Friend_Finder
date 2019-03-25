e992f15373faa1efc7bdab242ab385fbf293c5cf
Aaron.Gauntt <gauntta@gmail.com>
2019-03-24 20:44:51 -0500

    lets try this

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express(); 
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parlse applicaiton body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+app.listen(PORT, function() {
+    console.log("server listening on: http://localhost:" + PORT);
+})
+
